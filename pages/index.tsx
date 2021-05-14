8c8ccda9b8d6b2109204ea2f1d71cb47bcf41263
Mateusz Sokola <mateuszsokola@gmail.com>
2021-05-14 18:45:17 +0400

    feat: hook new feature toggle service

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,27 @@ import { Layout } from "antd";
 
 import { GdpChart, TreasuryChart } from "../components/Charts";
 import { useFeatureToggle } from "../hooks/useFeatureToggle";
+import { FeatureArray, fetchFeatures } from "../services/FeatureToggle";
 
 const { Header, Content } = Layout;
 
 export default function Home() {
-  const [isEnabled] = useFeatureToggle();
+  const [isEnabled, enableFeature, disableFeature] = useFeatureToggle();
+
+  const processFeatures = useCallback(async () => {
+    try {
+      const features = await fetchFeatures();
+      features.forEach(([featureName, isEnabled]: FeatureArray) =>
+        isEnabled ? enableFeature(featureName) : disableFeature(featureName)
+      );
+    } catch (_) {
+      // do nothing
+    }
+  }, [enableFeature, disableFeature]);
+
+  useEffect(() => {
+    processFeatures();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <Layout className="layout">
