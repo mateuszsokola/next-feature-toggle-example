701bf7d53a5829566094defe91efe59232495657
Mateusz Sokola <mateuszsokola@gmail.com>
2021-05-14 18:43:10 +0400

    feat: remove existing feature toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,8 @@ import { FeatureToggle } from "../components/FeatureToggleProvider";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  const enabledFeatures = [
-    process.env.NEXT_PUBLIC_FEATURE_TREASURY_CHART ? "treasury_chart" : "",
-  ].filter(feature => feature !== '');
-
   return (
-    <FeatureToggle enabledFeatures={enabledFeatures}>
+    <FeatureToggle>
       <Component {...pageProps} />
     </FeatureToggle>
   );
